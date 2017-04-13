7a295a94aa7f7896a283d06cb619b798b3f3958d
Eryn Li <erynli@utexas.edu>
2017-04-13 14:16:52 -0500

    added Parser files from p3 and main to test CSR

---
+++ src/Parser.cpp
@@ -0,0 +1,22 @@
+#include "Parser.h"
+
+Parser::Parser(int32_t sourceNode) : src(sourceNode) {}
+
+CSR Parser::parseInput(){
+    //Extract info from first line
+    string s;
+    int32_t size, edges, x, y, val;
+    cin >> s >> s >> size >> edges;
+
+    //Construct CSR as we read the file
+    CSR csr = CSR(size, edges, src);
+    for(int i = 0; i < edges; ++i){
+        cin >> s >> x >> y >> val;
+        if(s == "a") {
+            csr.put(x, y, val);
+        }
+    }
+    csr.update(++x, size + 1);
+
+    return csr;
+}
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "CSR.h"
+
+int main(){
+    clock_t t;
+    Parser p = Parser();
+    CSR csr = p.parseInput();
+    t = clock() - t;
+    cout << "CSR construction takes " << ((float)t)/CLOCKS_PER_SEC << " seconds" << endl;
+
+    return 0;
+}
